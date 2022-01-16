9ed340f3f9b02c7b5494ac1b350412d00dc1f84e
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-16 12:04:43 -0300

    Implementando o ToString

--- DIO.Series/Classes/Serie.cs
+++ DIO.Series/Classes/Serie.cs
@@ -7,9 +7,33 @@ namespace DIO.Series
 {
     public class Serie : EntidadeBase
     {
-        public Generd Generd { get; set; }
-        public string Titulo { get; set; }
-        public string Descricao { get; set; }
-        public int Ano { get; set; }
+        //Atrinbutos
+
+        private Generd Generd { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        //Metodos
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            // Enviroment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view-netcore-3.1
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Enviroment.NewLine;
+            retorno += "Tilulo: " + this.Tilulo + Enviroment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Enviroment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano;
+            return retorno;
+        }
     }
 }
